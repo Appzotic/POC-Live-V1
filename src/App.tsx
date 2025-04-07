7b7564ad7fb29bb32a4bc07313c8cabcb030280d
Sultan Khan <skcloud21@gmail.com>
2025-04-07 19:34:08 +0530

    made the ts configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import  { useState, useEffect } from 'react';
 import Scoreboard from './components/Scoreboard';
 import './styles/Scoreboard.css';
 
